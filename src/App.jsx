5994c3c30837310b21b5f1a058885ef99f033bc6
Sanskar Jain <sanskarj15@gmail.com>
2022-02-02 20:17:38 +0530

    build add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { React} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import FeedbackList from "./components/FeedbackList";
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { StrictMode, React } from "react";
 import ReactDom from "react-dom";
 import App from "./App.jsx"
 import "./index.css"
